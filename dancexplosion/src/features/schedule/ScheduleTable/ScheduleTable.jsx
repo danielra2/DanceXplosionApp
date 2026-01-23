dac9d46f41d2109220a5cc66ccaaee87db710aa1
Horatiu Cretu <Horatiu.Cl.Cretu@student.utcluj.ro>
2026-01-23 09:24:55 +0200

    changed from Ady to Adi

--- dancexplosion/src/features/schedule/ScheduleTable/ScheduleTable.jsx
+++ dancexplosion/src/features/schedule/ScheduleTable/ScheduleTable.jsx
@@ -13,7 +13,7 @@ const scheduleData = [
                 { time: "20:00", name: "Kizomba Improver", instr: "Cris", level: "Intermediari" }
             ],
             "Marți": [
-                { time: "18:00", name: "Copii 7-9 Ani (Grupa A)", instr: "Ady", level: "Copii" },
+                { time: "18:00", name: "Copii 7-9 Ani (Grupa A)", instr: "Adi", level: "Copii" },
                 { time: "19:00", name: "Salsa Intermediari - Avansați", instr: "Alex M - Cris", level: "Intermediari/Avansați" },
                 { time: "20:00", name: "Bachata Improver", instr: "Alex L", level: "Intermediari" },
                 { time: "21:00", name: "Trupa DXS", instr: "", level: "Performanță" }
@@ -25,7 +25,7 @@ const scheduleData = [
                 { time: "21:00", name: "Trupa DXS", instr: "", level: "Performanță" }
             ],
             "Joi": [
-                { time: "18:00", name: "Copii 7-9 Ani (Grupa A)", instr: "Ady", level: "Copii" },
+                { time: "18:00", name: "Copii 7-9 Ani (Grupa A)", instr: "Adi", level: "Copii" },
                 { time: "19:00", name: "Salsa Intermediari - Avansați", instr: "Alex M + Cris", level: "Intermediari/Avansați" },
                 { time: "20:00", name: "Bachata Improver", instr: "Alex L", level: "Intermediari" },
                 { time: "22:00", name: "Practice Party", instr: "", level: "Acces Liber" }
@@ -54,26 +54,26 @@ const scheduleData = [
             "Luni": [
                 { time: "17:00", name: "Copii 4-6 Ani", instr: "Cris", level: "Copii" },
                 { time: "19:00", name: "Copii 10-14 Ani", instr: "Alex", level: "Copii" },
-                { time: "20:00", name: "Curs Studenți", instr: "Ady + Allisor", level: "Acces Liber" }
+                { time: "20:00", name: "Curs Studenți", instr: "Adi + Allisor", level: "Acces Liber" }
             ],
             "Marți": [
-                { time: "17:00", name: "Copii 7-9 Ani", instr: "Ady", level: "Copii" },
+                { time: "17:00", name: "Copii 7-9 Ani", instr: "Adi", level: "Copii" },
                 { time: "18:00", name: "Copii 4-6 Ani", instr: "Allisor", level: "Copii" },
                 { time: "19:00", name: "Începători Mixt", instr: "Alex L + Allisor", level: "Start 20 Ianuarie" },
-                { time: "20:00", name: "Bachata Intermediari", instr: "Ady + Allisor", level: "Intermediari" },
+                { time: "20:00", name: "Bachata Intermediari", instr: "Adi + Allisor", level: "Intermediari" },
                 { time: "21:00", name: "Trupa DXS", instr: "", level: "Performanță" }
             ],
             "Miercuri": [
                 { time: "17:00", name: "Copii 4-6 Ani", instr: "Cris", level: "Copii" },
                 { time: "19:00", name: "Copii 10-14 Ani", instr: "Alex L", level: "Copii" },
-                { time: "20:00", name: "Curs Studenți", instr: "Ady + Allisor", level: "Acces Liber" },
+                { time: "20:00", name: "Curs Studenți", instr: "Adi + Allisor", level: "Acces Liber" },
                 { time: "21:00", name: "Trupa DXS", instr: "", level: "Performanță" }
             ],
             "Joi": [
-                { time: "17:00", name: "Copii 7-9 Ani", instr: "Ady", level: "Copii" },
+                { time: "17:00", name: "Copii 7-9 Ani", instr: "Adi", level: "Copii" },
                 { time: "18:00", name: "Copii 4-6 Ani", instr: "Allisor", level: "Copii" },
                 { time: "19:00", name: "Începători Mixt", instr: "Alex L + Allisor", level: "Start 20 Ianuarie" },
-                { time: "20:00", name: "Bachata Intermediari", instr: "Ady + Allisor", level: "Intermediari" },
+                { time: "20:00", name: "Bachata Intermediari", instr: "Adi + Allisor", level: "Intermediari" },
                 { time: "22:00", name: "Practice Party", instr: "", level: "Acces Liber" }
             ],
             "Vineri": []
